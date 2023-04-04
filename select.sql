808287aab1cafb99ea6847dbf4631e3e3573e31a
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-04 13:48:27 +0200

    fix selects

--- select.sql
+++ select.sql
@@ -1,19 +1,40 @@
 -- list every person's insurance (person_name, person_surname, person_personal_id, insurance_name, insurance_policy_number)
-SELECT p.person_name, p.person_surname, p.person_personal_id, i.insurance_name, i.insurance_policy_number
-FROM person p
-INNER JOIN insurance i
-    ON p.person_id = i.person_id;
+SELECT
+    P.PERSON_NAME,
+    P.PERSON_SURNAME,
+    P.PERSON_PERSONAL_ID,
+    I.INSURANCE_NAME,
+    I.INSURANCE_POLICY_NUMBER
+FROM
+    PERSON    P
+    INNER JOIN INSURANCE I
+    ON P.PERSON_INSURANCE_ID = I.INSURANCE_ID;
 
 -- list every vehicles' insurance (motor_vehicle_license_plate, motor_vehicle_make, motor_vehicle_model, motor_vehicle_year, insurance_name, insurance_policy_number)
-SELECT v.motor_vehicle_license_plate, v.motor_vehicle_make, v.motor_vehicle_model, v.motor_vehicle_year, i.insurance_name, i.insurance_policy_number
-FROM motor_vehicle v
-INNER JOIN insurance i
-    ON v.motor_vehicle_insurance_id = i.insurance_id;
+SELECT
+    V.MOTOR_VEHICLE_LICENSE_PLATE,
+    V.MOTOR_VEHICLE_MAKE,
+    V.MOTOR_VEHICLE_MODEL,
+    V.MOTOR_VEHICLE_YEAR,
+    I.INSURANCE_NAME,
+    I.INSURANCE_POLICY_NUMBER
+FROM
+    MOTOR_VEHICLE V
+    INNER JOIN INSURANCE I
+    ON V.MOTOR_VEHICLE_INSURANCE_ID = I.INSURANCE_ID;
 
 -- list every vehicle's owner and their motor vehicle (person_name, person_surname, person_personal_id, motor_vehicle_license_plate, motor_vehicle_make, motor_vehicle_model, motor_vehicle_year)
-SELECT p.person_name, p.person_surname, p.person_personal_id, v.motor_vehicle_license_plate, v.motor_vehicle_make, v.motor_vehicle_model, v.motor_vehicle_year
-FROM person p
-INNER JOIN vehicle_owner v_o
-    ON p.person_id = v_o.person_id
-INNER JOIN motor_vehicle v
-    ON v_o.motor_vehicle_id = v.motor_vehicle_id;
+SELECT
+    P.PERSON_NAME,
+    P.PERSON_SURNAME,
+    P.PERSON_PERSONAL_ID,
+    V.MOTOR_VEHICLE_LICENSE_PLATE,
+    V.MOTOR_VEHICLE_MAKE,
+    V.MOTOR_VEHICLE_MODEL,
+    V.MOTOR_VEHICLE_YEAR
+FROM
+    PERSON        P
+    INNER JOIN VEHICLE_OWNER V_O
+    ON P.PERSON_ID = V_O.VEHICLE_OWNER_PERSON_ID
+    INNER JOIN MOTOR_VEHICLE V
+    ON V_O.VEHICLE_OWNER_ID = V.MOTOR_VEHICLE_VEHICLE_OWNER_ID;
