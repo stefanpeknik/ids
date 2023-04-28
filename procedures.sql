9df6289324ce0aa0f70bdc6c9217abe826aa2f3e
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-28 21:33:02 +0200

    add procedure

--- procedures.sql
+++ procedures.sql
@@ -20,3 +20,30 @@ EXCEPTION
             || SQLERRM);
 END;
 /
+
+CREATE OR REPLACE PROCEDURE get_driver_vehicle_info(
+    driver_id_in IN INT,
+    vehicle_make_out OUT VARCHAR2,
+    vehicle_model_out OUT VARCHAR2,
+    vehicle_year_out OUT INT,
+    vehicle_license_plate_out OUT VARCHAR2
+)
+IS
+    -- Declare the cursor
+    CURSOR driver_vehicle_cursor IS
+        SELECT MV.MOTOR_VEHICLE_MAKE, MV.MOTOR_VEHICLE_MODEL, MV.MOTOR_VEHICLE_YEAR, MV.MOTOR_VEHICLE_LICENSE_PLATE
+        FROM MOTOR_VEHICLE MV
+        INNER JOIN DRIVER D ON MV.MOTOR_VEHICLE_ID = D.DRIVER_ID
+        WHERE D.DRIVER_ID = driver_id_in;
+
+BEGIN
+    -- Open the cursor
+    OPEN driver_vehicle_cursor;
+    
+    -- Fetch the results into the output variables
+    FETCH driver_vehicle_cursor INTO vehicle_make_out, vehicle_model_out, vehicle_year_out, vehicle_license_plate_out;
+
+    -- Close the cursor
+    CLOSE driver_vehicle_cursor;
+END;
+/
